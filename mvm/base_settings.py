d0abb6a2c44240ba653511ee1583862a8f23407a
luisferyimi19 <luisdaviladeleon@gmail.com>
2025-08-11 12:13:26 -0600

    add config

--- mvm/base_settings.py
+++ mvm/base_settings.py
@@ -69,7 +69,7 @@ else:
 SECRET_KEY = SECRETS["MVM_APP_SECRET_KEY"]
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = SECRETS["MVM_APP_DEBUG"]
+DEBUG = False
 
 ALLOWED_HOSTS = SECRETS["MVM_APP_ALLOWED_HOSTS"]
 
